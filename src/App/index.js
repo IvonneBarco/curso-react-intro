4f33f414ae3712fb5570a65215ad908c9b6e89f2
Ivonne C. Barco <ivonne.barco@loganitsolutions.com>
2024-02-27 10:33:42 -0500

    TodoProvider

--- src/App/index.js
+++ src/App/index.js
@@ -1,59 +1,15 @@
-import { useState } from 'react';
 import { TodoProvider } from '../TodoContext';
 import './App.css';
 import { AppUI } from './AppUI';
 
+
 function App() {
 
-  const [state, setState] = useState('estado compartido');
   return (
-    <>
-      <TodoHeader>
-        <TodoCounter />
-        <TodoSearch />
-      </TodoHeader>
-      <TodoList>
-        <TodoItem state={state} />
-      </TodoList>
-    </>
+    <TodoProvider>
+      <AppUI />
+    </TodoProvider>
   );
 }
-
-function TodoHeader({ children }) {
-  return (
-    <header>
-      {children}
-    </header>
-  );
-}
-
-function TodoList({ children }) {
-  return (
-    <section className='TodoList-container'>
-      {children}
-    </section>
-  );
-}
-
-function TodoCounter() {
-  return <p>TodoCounter</p>;
-}
-
-
-function TodoSearch() {
-  return <p>Todo Search</p>;
-}
-
-function TodoItem({ state }) {
-  return <p>Todo Item: {state}</p>;
-}
-// function App() {
-
-//   return (
-//     <TodoProvider>
-//       <AppUI />
-//     </TodoProvider>
-//   );
-// }
 
 export default App;
